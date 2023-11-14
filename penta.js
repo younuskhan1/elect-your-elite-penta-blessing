bd83c3643079b74e80d165202d8f372aa04cd5ff
younuskhan1 <md.younuskhan412@gmail.com>
2023-11-14 15:00:07 +0600

    total cost button task is completed but valiation is just left now

--- penta.js
+++ penta.js
@@ -1,11 +1,15 @@
 const buttons = document.getElementsByClassName("select-button");
+
 let count = 0;
+let validCount;
+
 for (let btn of buttons) {
     btn.addEventListener("click", function (event) {
         count++;
         const targetPlayer = event.target.parentNode.parentNode.childNodes[3].innerText;
         const selectedPlayerContainer = document.getElementById("selected-player-container");
         if (count <= 5) {
+            validCount = count;
             const li = document.createElement("li");
             li.innerText = `${count}. ${targetPlayer}`;
             li.classList.add("selected-player-name");
@@ -27,8 +31,26 @@ function getInputValueById(inputFieldId) {
     return inputValueNumber;
 }
 
+function getTextElementById(textFieldId) {
+    const textField = document.getElementById(textFieldId);
+    const inputValueString = textField.innerText;
+    const textElementNumber = parseFloat(inputValueString);
+    return textElementNumber;
+}
+
+
 document.getElementById("calculate-button").addEventListener("click", function () {
     const perPlayerRate = getInputValueById("per-player-cost");
-    const costOfplayers = perPlayerRate * count;
-    document.getElementById("player-expenses").innerText = costOfplayers;
+    const costOfPlayers = perPlayerRate * validCount;
+    document.getElementById("player-expenses").innerText = costOfPlayers;
+})
+
+
+
+document.getElementById("total-cost-button").addEventListener("click", function () {
+    const totalPlayersExpenses = getTextElementById("player-expenses");
+    const managerExpenses = getInputValueById("manager-expenses");
+    const coachExpenses = getInputValueById("coach-expenses");
+    const totalAmountCost = totalPlayersExpenses + managerExpenses + coachExpenses;
+    document.getElementById("total-expenses").innerText = totalAmountCost;
 })
