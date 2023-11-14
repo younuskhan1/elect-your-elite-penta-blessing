cdc55e78cb7cf8fa4230eeea0edeaaf90b8b081b
younuskhan1 <md.younuskhan412@gmail.com>
2023-11-14 13:20:58 +0600

    selected Players section task is totally completed

--- penta.js
+++ penta.js
@@ -1,9 +1,20 @@
 const buttons = document.getElementsByClassName("select-button");
+let count = 0;
 for (let btn of buttons) {
     btn.addEventListener("click", function (event) {
-        const target = event.target.parentNode.parentNode.childNodes[3].innerText;
-        console.log(target);
-        const buttonDisable = event.target;
-        buttonDisable.setAttribute("disabled", true);
+        count++;
+        const targetPlayer = event.target.parentNode.parentNode.childNodes[3].innerText;
+        const selectedPlayerContainer = document.getElementById("selected-player-container");
+        if (count <= 5) {
+            const li = document.createElement("li");
+            li.innerText = `${count}. ${targetPlayer}`;
+            li.classList.add("selected-player-name")
+            selectedPlayerContainer.appendChild(li);
+            const buttonDisable = event.target;
+            buttonDisable.setAttribute("disabled", true);
+        } else {
+            alert("You cannot add more than players at this moment.")
+        }
+
     })
 }
